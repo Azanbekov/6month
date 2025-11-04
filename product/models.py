342914f514f04a2f2a6bab6094dc65285ad8f6ba
Azanbekov <erzanazanbekov@gmail.com>
2025-11-04 23:28:28 +0600

    hw2 added

--- product/models.py
+++ product/models.py
@@ -1,9 +1,12 @@
 from django.db import models
+from users.models import CustomUser
 
 class Product(models.Model):
     name = models.CharField(max_length=100)
     price = models.DecimalField(max_digits=10, decimal_places=2)
     description = models.TextField(blank=True)
+    owner = models.ForeignKey(CustomUser, on_delete=models.CASCADE, related_name='products', null=True, blank=True)
+
 
     def __str__(self):
         return self.name
--- product/views.py
+++ product/views.py
@@ -1,7 +1,14 @@
 from rest_framework import viewsets
 from .models import Product
 from .serializers import ProductSerializer
+from .permissions import IsModerator
+from rest_framework.permissions import IsAuthenticatedOrReadOnly
 
 class ProductViewSet(viewsets.ModelViewSet):
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
+    permission_classes = [IsAuthenticatedOrReadOnly, IsModerator]
+
+    def perform_create(self, serializer):
+        serializer.save(owner=self.request.user)
+
